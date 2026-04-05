b9ef910ff2cd4d8090160652a8601eaaa16d2d9b
levvietdung3000-collab <levvietdung3000@gmail.com>
2026-04-05 20:59:14 +0700

    bài 16

---
+++ lab02/DigitalVideoDisc.java
@@ -0,0 +1,60 @@
+public class DigitalVideoDisc {
+   
+  private static int nbDigitalVideoDiscs = 0;
+
+    private int id;
+
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        updateId();
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        updateId();
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+        updateId();
+    }
+
+    // 4. Khởi tạo đầy đủ các thuộc tính
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+        updateId();
+    }
+
+ 
+    private void updateId() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
+    public String getTitle() { return title; }
+    public String getCategory() { return category; }
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+    public float getCost() { return cost; }
+    public int getId() { return id; }
+
+  
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
